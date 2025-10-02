e5395fad8a5f9092cbda33e2c3f0c21788d38aec
Abner <qw0207060413@gmail.com>
2025-10-03 02:17:15 +0800

    sync dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,5 @@
+# this dotfile refer to: https://github.com/JaKooLit/Hyprland-Dots
+
 cd $HOME
 
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -12,11 +14,24 @@ dot add .zshrc .zprofile .p10k.zsh
 dot add .icons .config/gtk-3.0
 
 # configs
+# https://github.com/JaKooLit/Hyprland-Dots/tree/main/config
 dot add .config/nvim
 dot add .config/kitty
 dot add .config/electron-flags.conf
 dot add .config/hypr
 dot add .config/waybar
+dot add .config/Kvantum
+dot add .config/quickshell
+dot add .config/rofi
+dot add .config/btop
+dot add .config/fastfetch
+dot add .config/qt5ct
+dot add .config/qt6ct
+dot add .config/swappy
+dot add .config/swaync
+dot add .config/wallust
+dot add .config/wlogout
+
 # .config/code-flags.conf
 
 dot commit -m "sync dotfiles"
